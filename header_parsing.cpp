eaee98a138349f05450395c879cc7062c91f3ca1
Nathan Colin <nathancolin27@gmail.com>
2021-08-06 16:28:58 +0200

    Pseudo code for request parsing

---
+++ header_parsing.cpp
@@ -0,0 +1,29 @@
+struct req_info {
+	// will contain all the information parsed
+	// method 
+	// host
+	//
+}
+
+void getMethod()
+{}
+
+void parseHeaderFields()
+{}
+
+void validHeaderFormat()
+{
+	// Http version 1.1 ?
+	// GET, POST or DEL Method
+	// no empty lines
+	// all the fields are valid fields -> check in a list of authorized fields
+}
+
+void request_parser(string request)
+{
+	
+	// separate header and body
+	// check validity of header
+	// parse fields into struct
+	// once completed, if everything went well -> parse body
+}
