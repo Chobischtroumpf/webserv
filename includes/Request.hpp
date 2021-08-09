1318c2b9000497d741e11578bb6e681478b17194
Nathan Colin <nathancolin27@gmail.com>
2021-08-09 14:18:03 +0200

    added _raw and _path + copy constructor

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -7,13 +7,17 @@ class HttpRequest
 		std::string							_version;		// Should always be "HTTP/1.1" of  "HTTP/1.0", otherwise -> Bad Request
 		std::map<std::string, std::string>	_headers;		// < Key, Value>
 		std::string							_body;			// Everything that is after a CRLF 
+		std::string							_raw;			// Whole request
+		std::string							_path;			// Path to requested 
 
 	public:
 		HttpRequest();
-		HttpRequest(const std::string req);
+		HttpRequest(std::string req);
 		HttpRequest(const HttpRequest &other);
 		~HttpRequest();
 		
+		HttpRequest& 						operator=(const HttpRequest &other);
+
 		void								ParseMethodVersion();
 		void								ParseHeader();
 		void								ParseBody();
@@ -22,5 +26,6 @@ class HttpRequest
 		std::string							GetVersion() const;
 		std::map<std::string, std::string>	GetHeaders() const;
 		std::string							GetBody() const;
+		std::string							GetRaw() const;
 		
 };
--- srcs/Request.cpp
+++ srcs/Request.cpp
@@ -9,16 +9,20 @@ HttpRequest&	HttpRequest::operator=(const HttpRequest & other) {
 	if (this != &other)
 	{
 		this->_method = other.GetMethod();
-		this->_headers = other.GetHeaders();
 		this->_version = other.GetVersion();
+		this->_headers = other.GetHeaders();
 		this->_body = other.GetBody();
+		this->_raw = other.GetRaw();
 	}
     return *this;
 }
 
 HttpRequest::HttpRequest(std::string req)
 {
-
+	this->_raw = req;
+	this->_method = "";
+	this->_version = "";
+	this->_body = "";
 }
 
 void HttpRequest::ParseMethodVersion()
@@ -28,10 +32,31 @@ void HttpRequest::ParseMethodVersion()
 
 void HttpRequest::ParseHeader()
 {
-
+	// Get first word, should be GET, POST or DELETE, otherwise 400, stop parsing
+	// Get path
 }
 
 void HttpRequest::ParseBody()
 {
 
+}
+
+std::string		HttpRequest::GetMethod() const
+{
+	return this->_method;
+}
+
+std::string		HttpRequest::GetBody() const
+{
+	return this->_body;
+}
+
+std::string		HttpRequest::GetVersion() const
+{
+	return this->_version;
+}
+
+std::string		HttpRequest::GetRaw() const
+{
+	return this->_raw;
 }
